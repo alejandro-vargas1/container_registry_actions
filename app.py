4e82492d3eb381b33a0c4d78ff4004ad815e34d4
Alejandro Vargas <alejandrovargas@esvyda.com>
2024-04-20 12:38:26 -0500

    Changed the host

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/hello")
+@app.route("/")
 def hello_world():
     return "Hello world"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="localhost", port=8080)
